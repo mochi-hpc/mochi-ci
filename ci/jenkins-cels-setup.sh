3b442d9e65551a3ed25ef61fc316cd9d7e07a831
Matthieu Dorier <mdorier@anl.gov>
2025-12-11 12:39:21 +0000

    fixing environment

--- ci/jenkins-cels-setup.sh
+++ ci/jenkins-cels-setup.sh
@@ -16,6 +16,3 @@ export SPACK_USER_CACHE_PATH=$HOME/spack-cache
 echo "Setting up Spack"
 git clone https://github.com/spack/spack.git
 . spack/share/spack/setup-env.sh
-
-echo "Cloning Mochi spack packages"
-git clone https://github.com/mochi-hpc/mochi-spack-packages.git
